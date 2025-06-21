6b7ed82c4ae67000693356fa0ece3df9456da7f9
Igor Sakuma <igorsakuma@touchtec.com.br>
2025-06-21 00:27:03 -0300

    implemented NPB-CHANNEL's EP and improved the README.md

---
+++ NPB-CHANNEL/EP/params/classWParam.go
@@ -0,0 +1,11 @@
+//go:build W
+
+package params
+
+const (
+	CLASS           = "W"
+	M               = 25
+	EmptyTag        = false
+	SX_VERIFY_VALUE = -2.863319731645753e+3
+	SY_VERIFY_VALUE = -6.320053679109499e+3
+)
